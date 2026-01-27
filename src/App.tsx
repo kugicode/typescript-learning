5c0b26aefd1fa470a95892d3608dc68379e6b2ad
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-01-27 22:09:33 +0000

    learning more typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-interface User {
-  name: string;
-  age: number;
-  isStudent: boolean;
-  role: "admin" | "user";
-  bio?: string;
+interface Post {
+  username: string;
+  content: string;
+  likes: number;
+  isPublished: boolean;
+  category: "news" | "manga" | "code"
 }
 
-function greet (name: string): string {
-  return "Hello " + name
+function getPostStatus(status: boolean): string{
+  if(status){
+    return "Public";
 }
-  
+else{
+  return "Private";
+}
+}
+
 function App() {
-const hobbies: string[] = ["coding", "manga"];
-const me: User = {name: "anas", age: 22, isStudent: true, role: "admin", bio: "..."}
+const posts :Post[] = [
+{username: "anas", content: "DQDai", likes: 22, isPublished: true, category: "manga"},
+{username: "taikobo", content: "hoshin", likes: 44, isPublished: true, category: "manga"},
+{username: "popp", content: "javascript", likes: 55, isPublished: false, category: "code"},
+]
 
   return (
     <>
-  <p>My name is { me.name }!</p>
-  <p>I am { me.age } years old!</p>
-  <p>{ hobbies[0] }</p>
-  <p>{ greet(me.name) }</p>
+  {posts.map((m) => (
+    <div key={m.username}>
+    <p>{m.username}</p>
+    <p>{m.content}</p>
+    <p>{getPostStatus(m.isPublished)}</p>
+    </div>
+  ))}
+
+  
     </>
   )
 }
