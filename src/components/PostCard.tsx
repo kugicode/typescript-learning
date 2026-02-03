2c4a2b90fa54e83fb522910d0c2b0eadb914a59e
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-02-03 23:48:58 +0000

    Got edit button to work

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -22,6 +22,8 @@ function PostCard({ newPost }: { newPost: Post }){
   const [isBookmarked, setIsBookmarked] = useState<boolean>(false);
   const [comment, setComment] = useState<string>('');
   const [allComment, setAllComment] = useState <string[]>([]);
+  const [isEditting, setIsEditing] = useState <boolean>(false);
+  const [editableContent, setEditableContent] = useState <string>(newPost.content)
   const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => { //You do this instead when writing Typescript in js you'd do (e.target.value)
   setComment(e.target.value);
 }
@@ -36,10 +38,13 @@ setAllComment(allComment.filter((c) => c !== commentToDelete))
 
 }
 
+
   return(
     <div>
       <p>{newPost.username}</p>
       <p>{newPost.content}</p>
+      <button onClick={() => setIsEditing(true)}>Edit</button>
+      {isEditting ? ( <> <input value={editableContent} onChange={(e) => setEditableContent(e.target.value)} /> <button onClick={() => setIsEditing(false)}>Save</button>  </> )  : <p>{ editableContent }</p>}
       <p>{getPostStatus(newPost.isPublished)}</p>
       <button onClick={()=> setLikesCount(likesCount + 1)}>Like</button>
       { likesCount }
@@ -49,7 +54,8 @@ setAllComment(allComment.filter((c) => c !== commentToDelete))
         <input type="text" placeholder="type..." value={comment} onChange={ handleTextChange }/>
         <button onClick={handleAddComment}>Post comment</button>
         {allComment.map((m) => (
-        <p key={m}>{m} <button onClick={() => handleDeleteComment(m)}>x</button></p>
+        <p key={m}>{m} <button onClick={() => handleDeleteComment(m)}>x</button> 
+        </p>
        
         ))}
         <p>{ comment }</p>
